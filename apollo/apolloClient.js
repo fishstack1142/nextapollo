3d66082b4da8022bb9740635ba727e6e2a478539
fishstack1142 <fishstack1142@gmail.com>
2020-09-18 14:34:54 +0700

    save jwt to cookie

--- apollo/apolloClient.js
+++ apollo/apolloClient.js
@@ -1,40 +1,54 @@
-import { ApolloClient } from "apollo-client"
-import { createHttpLink } from "apollo-link-http"
-import { setContext } from "apollo-link-context"
-import { InMemoryCache } from "apollo-cache-inmemory"
-import fetch from "isomorphic-unfetch"
-import withApollo from "next-with-apollo"
+import { ApolloClient } from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import { setContext } from "apollo-link-context";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import fetch from "isomorphic-unfetch";
+import withApollo from "next-with-apollo";
 
-import { ApolloProvider } from "@apollo/react-hooks"
+import cookie from "cookie";
 
-const uri = "https://graphbasicserver.azurewebsites.net/graphql"
+import { ApolloProvider } from "@apollo/react-hooks";
 
-const httpLink = createHttpLink({ uri, fetch })
+const uri = "https://graphbasicserver.azurewebsites.net/graphql";
+
+const httpLink = createHttpLink({ uri, fetch });
 
 const authLink = setContext((_, { headers }) => {
   // Get token from localStorage
-  const token = JSON.parse(localStorage.getItem("jwt"))
+  //   const token = JSON.parse(localStorage.getItem("jwt"))
+  let cookies;
+  if (headers) {
+    cookies = cookie.parse(header.cookie || "");
+  }
+
+  if (typeof window !== "undefined") {
+    cookies = cookie.parse(document.cookie || "");
+  }
+
+  const token = (cookies && cookies.token) || "";
 
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : ""
-    }
-  }
-})
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
 
-export default withApollo(({ initialState }) => {
-  return new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache().restore(initialState || {})
-  })
-},
-{
+export default withApollo(
+  ({ initialState }) => {
+    return new ApolloClient({
+      link: authLink.concat(httpLink),
+      cache: new InMemoryCache().restore(initialState || {}),
+    });
+  },
+  {
     render: ({ Page, props }) => {
       return (
         <ApolloProvider client={props.apollo}>
           <Page {...props} />
         </ApolloProvider>
       );
-    }
-  })
+    },
+  }
+);
--- pages/signin.js
+++ pages/signin.js
@@ -1,11 +1,134 @@
-import React from 'react'
+import React, { useState } from "react";
+import Router from "next/router";
+import apolloClient from "../apollo/apolloClient";
+import styled from "styled-components";
+import { useMutation } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+import Cookies from 'js-cookie'
 
-function signin() {
-    return (
-        <div>
-            sign in
-        </div>
-    )
+const LOGIN = gql`
+  mutation LOGIN($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      user {
+        id
+        name
+        email
+        createdProducts{
+            id
+            description
+          }
+          carts{
+            id
+            product {
+              description
+              price
+              imageUrl
+            }
+            quantity
+          }
+      }
+      jwt
+    }
+  }
+`
+
+const Div = styled.div`
+  margin: 100px;
+  padding: 20px;
+  color: red;
+`;
+
+const Para = styled.p`
+  color: blue;
+  text-align: center;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+  width: 30%;
+`;
+
+const Input = styled.input`
+  margin: 5px;
+  height: 30px;
+  background-color: pink;
+`;
+
+const Button = styled.button`
+  background-color: red;
+  color: white;
+  padding: 9px;
+`;
+
+function SignInPage() {
+  const [userInfo, setUserInfo] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = async e => {
+    setUserInfo({
+      ...userInfo,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  //   console.log(userInfo);
+
+  const [login, { loading, error, data }] = useMutation(LOGIN, {
+    variables: { ...userInfo },
+    onCompleted: data => {
+      console.log(data);
+      if (data) {
+          Cookies.set('token', data.login.jwt)
+        setUserInfo({
+          email: "",
+          password: "",
+        });
+      }
+      console.log('completed')
+
+      console.log(error)
+      Router.push('/products')
+    },
+  });
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    try {
+      await login();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <Div>
+      <Form onSubmit={handleSubmit}>
+        <Para>sign in</Para>
+        <Input
+          type="email"
+          name="email"
+          placeholder="E-mail"
+          onChange={handleChange}
+          value={userInfo.email}
+        />
+        <Input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={handleChange}
+          value={userInfo.password}
+        />
+        <Button type="submit" disabled={loading}>
+          SUBMIT
+        </Button>
+      </Form>
+
+    </Div>
+  );
 }
 
-export default signin
+export default apolloClient(SignInPage);
